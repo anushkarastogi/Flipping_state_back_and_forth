6e5e306768401108514fd1a62f7a8085ec786476
Anushka Rastogi <97504525+anushkarastogi@users.noreply.github.com>
2022-11-27 11:17:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./styles.css";
+export default function App() {
+  /**
+   * Challenge:
+   * - Initialize state for `isGoingOut` as a boolean
+   * - Make it so clicking the div.state--value flips that
+   *   boolean value (true -> false, false -> true)
+   * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
+   */
+  const [isGoingOut, setIsGoingOut] = React.useState(true);
+  function changeMind() {
+    setIsGoingOut((prevSate) => !prevSate);
+  }
+
+  return (
+    <div className="state">
+      <h1 className="state--title">Do I feel like going out tonight?</h1>
+      <div onClick={changeMind} className="state--value">
+        <h1>{isGoingOut ? "Yes" : "No"}</h1>
+      </div>
+    </div>
+  );
+}
